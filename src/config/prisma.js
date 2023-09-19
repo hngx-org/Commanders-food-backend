1d6ba8dcb7984803142dbdc7701b37d59b60f5bd
omotega <tomoyibo@gmail.com>
2023-09-19 18:41:37 +0100

    fix:fixed wrong of prisma module

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,5 +1,17 @@
-const { PrismaClient } = require("prisma");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
+
+async function main() {
+  // Test it out to see if everything is working properly
+  const allUsers = await prisma.users.create({
+    data: {
+      email: "tada@gmail.com",
+    },
+  });
+  console.log(allUsers);
+}
+
+main().then(console.log).catch(console.log);
 
 module.exports = prisma;
