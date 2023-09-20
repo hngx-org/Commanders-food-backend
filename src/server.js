9ba50a46b183fe0e47230e60b0a21cd90efc2930
jc-coder <josephchimezie2003@gmail.com>
2023-09-20 13:02:45 +0100

    remove organisation route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const App = require('./app');
 const UserRoute = require('./routes/user');
 const LunchRoute = require('./routes/lunch');
-const OrganisationRoute = require('./routes/organisation');
 
 const server = new App();
-server.initializedRoutes([new UserRoute(), new LunchRoute(), new OrganisationRoute()]);
+server.initializedRoutes([new UserRoute(), new LunchRoute()]);
 server.listen();
