ea2fe305d44e6ad2d592a9725345d7833eddb70e
nodekage <anthonycuzoho@gmail.com>
2023-09-20 16:18:28 +0100

    feat : added getAllUsers Endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,3 @@
-const express = require("express");
-const UserController = require("../controller/user");
-const useCatchErrors = require("../error/catchErrors");
-
 class UserRoute {
   router = express.Router();
   userController = new UserController();
@@ -11,15 +7,13 @@ class UserRoute {
     this.initializeRoutes();
   }
 
-
   initializeRoutes() {
+    
     this.router.get(
       `${this.path}/data`, 
       useCatchErrors(this.userController.getUser.bind(this.userController))
     );
-  }
 
-  initializeRoutes() {
     // Endpoint to get all users
     this.router.get(
       `${this.path}/users`,
