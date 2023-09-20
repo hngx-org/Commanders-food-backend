66e3935b610223de5ed7c435e4af9cdd5be5f974
Marvis Alimhiantare <marvis.alimhiantare@gmail.com>
2023-09-20 17:07:13 +0100

    get-user-profile functionality

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,24 +1,55 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
 const BaseController = require("./base");
 
 class UserController extends BaseController {
-  constructor() {
-    super();
-  }
-  async getUser(req, res) {
-    const userdata = [
-      {
-        name: "john doe",
-        email: "john@mail.com",
-      },
-      {
-        name: "brain tracy",
-        email: "brian@mail.com",
-      },
-    ];
-    this.success(res, "user data fetched successfully", 200, userdata);
-  }
+	constructor() {
+		super();
+	}
+	async getUser(req, res) {
+		const userdata = [
+			{
+				name: "john doe",
+				email: "john@mail.com",
+			},
+			{
+				name: "brain tracy",
+				email: "brian@mail.com",
+			},
+		];
+		this.success(res, "user data fetched successfully", 200, userdata);
+	}
+
+	async getUserProfile(req, res) {
+		const { user_id } = req.params;
+
+		const user = await prisma.user.findUnique({
+			where: { id: user_id },
+		});
+
+		if (!user) {
+			const errorData = {
+				message: `User with id ${user_id} does not exist`,
+			};
+			this.error(res, "User Not Found", 404, errorData);
+		} else {
+			const userProfile = {
+				data: {
+					name: `${user.first_name} ${user.last_name}`,
+					email: user.email,
+					phonenumber: user.phonenumber,
+					profile_picture: user.profile_picture,
+					lunch_credit_balance: user.lunch_credit_balance,
+				},
+			};
+			this.success(
+				res,
+				"User data fetched successfully",
+				200,
+				userProfile
+			);
+		}
+	}
 }
 
 module.exports = UserController;
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,20 +3,29 @@ const UserController = require("../controller/user");
 const useCatchErrors = require("../error/catchErrors");
 
 class UserRoute {
-  router = express.Router();
-  userController = new UserController();
-  path = "/user";
+	router = express.Router();
+	userController = new UserController();
+	path = "/user";
 
-  constructor() {
-    this.initializeRoutes();
-  }
+	constructor() {
+		this.initializeRoutes();
+	}
 
-  initializeRoutes() {
-    this.router.get(
-      `${this.path}/data`,
-      useCatchErrors(this.userController.getUser.bind(this.userController))
-    );
-  }
+	initializeRoutes() {
+		this.router.get(
+			`${this.path}/data`,
+			useCatchErrors(
+				this.userController.getUser.bind(this.userController)
+			)
+		);
+		this.router.get(
+			`${this.path}/profile`,
+      isAuthenticated,
+			useCatchErrors(
+				this.userController.getUserProfile.bind(this.userController)
+			)
+		);
+	}
 }
 
 module.exports = UserRoute;
