0081c8adaf502a44dbdf9fc23cbf297810fd6685
Dearie <eburujacintha@gmail.com>
2023-09-21 14:23:46 +0100

    docs: working on documentation

--- src/documentation/swagger.js
+++ src/documentation/swagger.js
@@ -4,5 +4,13 @@ info:
   title: My Node.js API
   description: Documentation for My Node.js API
   version: 1.0.0
+
+  servers:
+  - url: http://api.example.com/v1
+    description: Optional server description, e.g. Main (production) server
+  - url: http://staging-api.example.com
+    description: Optional server description, e.g. Internal staging server for testing
+
+    
 `
 module.exports = swagger;
