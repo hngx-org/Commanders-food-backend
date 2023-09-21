ec4c1935056650613516e421e2ea9331cca1e921
Ayo <heyayomideadebisi@gmail.com>
2023-09-21 01:18:28 +0100

    feat: add withdrawal endpoint

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const UserRoute = require("./routes/user");
 const LunchRoute = require("./routes/lunch"); 
 const OrganizationRoute = require("./routes/organization");
 const AuthRoute = require("./routes/auth");
+const WithdrawalController = require("./routes/withdrawal")
 
 const server = new App();
 server.initializedRoutes([
@@ -10,5 +11,6 @@ server.initializedRoutes([
   new LunchRoute(),
   new AuthRoute(),
   new OrganizationRoute(),
+  new WithdrawalController(),
 ]);
 server.listen();
