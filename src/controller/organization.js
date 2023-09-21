4eda994d8e40f3e25e49c1f431f18da42e750c82
CammyJR <you@example.com>
2023-09-21 13:40:02 +0100

    feat: create organization endpoint #12

--- src/controller/organization.js
+++ src/controller/organization.js
@@ -1,9 +1,43 @@
 const BaseController = require("./base");
+const { OrganizationSchema } = require("../helper/validate");
+const prisma = require("../config/prisma");
+const shortId = require("short-uuid");
 
 class OrganizationController extends BaseController {
     constructor() {
         super();
     }
+
+    async createOrganization(req, res){
+        const payload = req.body;
+        
+        const { error } = OrganizationSchema.validate(payload);
+        if (error) {
+            return this.error(res, error.message, 400);
+        }
+
+        var {organization_name, lunch_price} = payload;
+        const id = shortId.generate();
+
+        if (!lunch_price) {
+            var lunch_price = "1000";
+        }
+        console.log("after", lunch_price);
+
+        // Create organization
+        await prisma.organization.create({
+            data: {
+                id: id,
+                name: organization_name,
+                lunch_price,
+            }
+        });
+
+        this.success(res, "success",200, {
+            message: "organization added sucessfully"
+        });
+    }
 }
+
 
 module.exports = OrganizationController;
