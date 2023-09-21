fe1f2951d7edfd377f800aa52d71f3d61dec0bbb
jc-coder <josephchimezie2003@gmail.com>
2023-09-21 22:48:01 +0100

    add feature to redeem user lunch by id and to redeem all user lunches

--- prisma/migrations/20230921204659_jc_migration/migration.sql
+++ prisma/migrations/20230921204659_jc_migration/migration.sql
@@ -4,21 +4,22 @@ CREATE TABLE `users` (
     `org_id` VARCHAR(191) NOT NULL,
     `first_name` VARCHAR(191) NOT NULL,
     `last_name` VARCHAR(191) NOT NULL,
-    `profile_picture` VARCHAR(191) NOT NULL,
+    `profile_picture` VARCHAR(191) NULL,
     `email` VARCHAR(191) NOT NULL,
-    `phonenumber` INTEGER NOT NULL,
+    `phonenumber` VARCHAR(191) NOT NULL,
     `password_hash` VARCHAR(191) NOT NULL,
-    `isAdmin` BOOLEAN NOT NULL,
-    `lunch_credit_balance` VARCHAR(191) NOT NULL,
-    `refresh_token` VARCHAR(191) NOT NULL,
-    `updated_at` DATETIME(3) NOT NULL,
-    `bank_number` VARCHAR(191) NOT NULL,
-    `bank_code` VARCHAR(191) NOT NULL,
-    `bank_name` VARCHAR(191) NOT NULL,
-    `created_at` DATETIME(3) NOT NULL,
+    `isAdmin` BOOLEAN NOT NULL DEFAULT false,
+    `lunch_credit_balance` VARCHAR(191) NULL DEFAULT '0',
+    `refresh_token` VARCHAR(255) NOT NULL,
+    `bank_number` VARCHAR(191) NULL,
+    `bank_code` VARCHAR(191) NULL,
+    `bank_name` VARCHAR(191) NULL,
+    `bank_region` VARCHAR(191) NULL,
+    `currency_code` VARCHAR(191) NULL,
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `users_id_key`(`id`),
-    UNIQUE INDEX `users_org_id_key`(`org_id`),
     INDEX `users_id_idx`(`id`),
     INDEX `users_org_id_idx`(`org_id`),
     PRIMARY KEY (`id`)
@@ -30,7 +31,7 @@ CREATE TABLE `withdrawals` (
     `user_id` VARCHAR(191) NOT NULL,
     `status` VARCHAR(191) NOT NULL,
     `amount` BIGINT NOT NULL,
-    `created_at` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `withdrawals_id_key`(`id`),
     INDEX `withdrawals_id_idx`(`id`),
@@ -41,9 +42,9 @@ CREATE TABLE `withdrawals` (
 -- CreateTable
 CREATE TABLE `organization` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `lunch_price` VARCHAR(191) NOT NULL,
-    `currency` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `lunch_price` VARCHAR(191) NULL,
+    `currency` VARCHAR(191) NULL,
 
     UNIQUE INDEX `organization_id_key`(`id`),
     PRIMARY KEY (`id`)
@@ -52,9 +53,9 @@ CREATE TABLE `organization` (
 -- CreateTable
 CREATE TABLE `organization_invites` (
     `id` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
-    `token` VARCHAR(191) NOT NULL,
-    `TTL` DATETIME(3) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `token` VARCHAR(191) NULL,
+    `TTL` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `organization_invites_id_key`(`id`),
     PRIMARY KEY (`id`)
@@ -69,11 +70,13 @@ CREATE TABLE `lunches` (
     `redeemed` BOOLEAN NOT NULL,
     `created_at` DATETIME(3) NOT NULL,
     `note` VARCHAR(191) NOT NULL,
+    `org_id` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `lunches_id_key`(`id`),
     INDEX `lunches_id_idx`(`id`),
     INDEX `lunches_senderId_idx`(`senderId`),
     INDEX `lunches_receiverId_idx`(`receiverId`),
+    INDEX `lunches_org_id_idx`(`org_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- src/routes/lunch.js
+++ src/routes/lunch.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const LunchController = require("../controller/lunch");
-const useCatchErrors = require("../error/catchErrors");
-const { isAuthenticated } = require("../middlewares/auth");
+const express = require('express');
+const LunchController = require('../controller/lunch');
+const useCatchErrors = require('../error/catchErrors');
+const { isAuthenticated } = require('../middlewares/auth');
 
 class LunchRoute {
   router = express.Router();
   lunchController = new LunchController();
-  path = "/lunch";
+  path = '/lunch';
 
   constructor() {
     this.initializeRoutes();
@@ -24,6 +24,24 @@ class LunchRoute {
       `${this.path}/send`,
       isAuthenticated,
       useCatchErrors(this.lunchController.sendLunch.bind(this.lunchController))
+    );
+
+    // redeem user lunch by id
+    this.router.put(
+      `${this.path}/redeem/:id`,
+      isAuthenticated,
+      useCatchErrors(
+        this.lunchController.redeemLunchById.bind(this.lunchController)
+      )
+    );
+
+    // redeem all user lunches
+    this.router.put(
+      `${this.path}/redeem`,
+      isAuthenticated,
+      useCatchErrors(
+        this.lunchController.redeemAllUserLunches.bind(this.lunchController)
+      )
     );
   }
 }
