2204e369ba589842bb8552d525fac4ad09b62dee
Olakunle Raheem <olaks.raheem@gmail.com>
2023-09-21 13:12:24 +0100

    Refactor: Changed status code

--- src/controller/lunch.js
+++ src/controller/lunch.js
@@ -181,7 +181,7 @@ class LunchController extends BaseController {
       responseData.push(data);
     }
 
-    return this.success(res, "Lunch transfer was successful", 200, responseData);
+    return this.success(res, "Lunch transfer was successful", 201, responseData);
   }
 }
 
