65bf44e4ba1cd55f2ce6a923865af46548cddcfd
Ezeah <ezeahnnamdi@gmail.com>
2023-09-21 04:38:01 +0100

    feat: modified user controller

--- src/controller/user.js
+++ src/controller/user.js
@@ -51,19 +51,41 @@ class UserController extends BaseController {
 		}
 	}
 
-  // retrieve all users within the organization
-  async allUsers(req, res) {
-    // authenticate request
-      const organizationId = req.user.org_id;
+	async updateUserBankDetails(req, res) {
+		// Assuming you have a route parameter for user ID
+		const userId = req.params.id;
+		//Assuming the updated bank details are sent in the request body
+		const updatedBankDetails = req.body; 
+	  
+		// Check if the user with the specified ID exists
+		const existingUser = await prisma.user.findUnique({
+		  where: { id: userId },
+		});
+		
+		if (!existingUser) {
+		  return this.error(res, "User not found", 404);
+		}
+	  
+		// Update the user's bank details
+		const updatedUser = await prisma.user.update({
+		  where: { id: userId },
+		// Assuming the bank details properties match the Prisma User model  
+		  data: updatedBankDetails, 
+		});
+		this.success(res, "User bank details updated successfully", 200, updatedUser);
+	}
 
-      // Retrieve all users within the organization
-      const users = await prisma.user.findMany({
-        where: {
-          organization: {
-            id: organizationId,
-          },
+    // retrieve all users within the organization
+    async allUsers(req, res) {
+        // authenticate request
+        const organizationId = req.user.org_id;
+
+        // Retrieve all users within the organization
+        const users = await prisma.user.findMany({
+          where: {
+            organization: { id: organizationId },
         },
-      });
+    });
 
       //response payload
       const payload = {
