aa033851ac27de76f1cd94260588d57aa69de495
Benrobo <alumonabenaiah71@gmail.com>
2023-09-22 02:51:41 +0100

    fix merge conflict

--- src/controller/organization.js
+++ src/controller/organization.js
@@ -162,6 +162,32 @@ class OrganizationController extends BaseController {
       process.env.NODE_ENV !== "production" && otp
     );
   }
+
+  async updateOrganizationInfo(req, res) {
+    const { org_id } = req.user;
+    const payload = req.body;
+
+    //validating payload
+    const { error } = OrganizationSchema.validate(payload);
+    if (error) {
+      return this.error(res, error.message, 400);
+    }
+
+    const { organization_name, lunch_price } = payload;
+
+    //update to database
+    await prisma.organization.update({
+      where: {
+        id: org_id,
+      },
+      data: {
+        name: organization_name,
+        lunch_price: String(lunch_price),
+      },
+    });
+
+    this.success(res, "success", 200);
+  }
 }
 
 module.exports = OrganizationController;
