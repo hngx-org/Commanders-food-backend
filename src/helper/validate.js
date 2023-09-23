819329b8ce3f2afed9a54ea7d385421eacebc93e
Benrobo <alumonabenaiah71@gmail.com>
2023-09-23 12:49:41 +0100

    updated validate

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -56,8 +56,12 @@ const OrganizationUpdateSchema = Joi.object({
 });
 
 const passwordResetSchema = Joi.object({
+  new_password: Joi.string().min(6).required(),
+  otp_code: Joi.string().required(),
+});
+
+const ForgotPasswordSchema = Joi.object({
   email: Joi.string().required(),
-  password: Joi.string().min(6).required(),
 });
 
 module.exports = {
@@ -71,4 +75,5 @@ module.exports = {
   saveBankInfoShema,
   OrganizationUpdateSchema,
   passwordResetSchema,
+  ForgotPasswordSchema,
 };
